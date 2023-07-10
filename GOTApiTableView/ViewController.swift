508dfbb41332ce92c1f1989d76ed88b4f930167d
randy7394 <randymorales17@gmail.com>
2023-07-09 21:38:34 -0600

    feat: Crear funcion que llama los datos de la api

--- GOTApiTableView/ViewController.swift
+++ GOTApiTableView/ViewController.swift
@@ -11,9 +11,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getData() { result in
+            print(result)
+        }
     }
 
-
+    func getData(completion: @escaping ((APIModel) -> Void)) {
+        
+        guard let url = URL(string: "https://thronesapi.com/api/v2/Characters") else {
+            return
+        }
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: URLRequest(url: url)) { data, response, error in
+            if data != nil && error == nil {
+                do {
+                    let decoder = JSONDecoder()
+                    let response = try decoder.decode(APIModel.self, from: data!)
+                    completion(response)
+                } catch {
+                    print(String(describing: error))
+                }
+            }
+        }
+        task.resume()
+    }
 }
 

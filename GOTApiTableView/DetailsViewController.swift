dc339280fde5297092f8a0c778dbb711829d9470
randy7394 <randymorales17@gmail.com>
2023-07-12 20:58:07 -0600

    chore:Agregar contraints a elementos de la vista

---
+++ GOTApiTableView/DetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailsViewController.swift
+//  GOTApiTableView
+//
+//  Created by Randy Morales on 12/7/23.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet var characterImage: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var familyNameLabel: UILabel!
+    @IBOutlet var tittleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
